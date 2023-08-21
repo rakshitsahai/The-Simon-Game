667ba2ef360a4f5cf39485acc289fa9cc3afe9f3
Rakshit <rakshitsahai@gmail.com>
2023-08-21 14:23:34 +0530

    added sounds to button clicks

--- game.js
+++ game.js
@@ -2,14 +2,14 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
-
   userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
 
 });
 
@@ -21,6 +21,10 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
